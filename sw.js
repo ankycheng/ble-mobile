fbf795faeec1602d697727f7889e6832d5076b45
ankycheng <ankycheng@gmail.com>
2025-01-28 13:28:17 -0500

    Update: add sw.js for pwa

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Change this to your repository name
+var GHPATH = '/ble-mobile';
+ 
+// Choose a different app prefix name
+var APP_PREFIX = 'ble_mobile_';
+ 
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+ 
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/static/manifest.json`,
+  `${GHPATH}/offline.html`,
+  `${GHPATH}/p5.js`,
+  `${GHPATH}/p5.ble.js`,
+  `${GHPATH}/sketch2.js`,
+  `${GHPATH}/style.css`,
+]
